0c544b1fc78ab5d7596a8e50271655fd9afbb057
agita-karklite <vernere.agita@gmail.com>
2022-05-10 21:53:12 +0300

    add extra examples

---
+++ src/main/java/classroom/MainTwo.java
@@ -0,0 +1,18 @@
+package classroom;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        String vards = "Agita";
+        String uzvards = "Karklite";
+        int vecums = 29;
+        boolean isPresent = true;
+
+        System.out.println("Vārds: " + vards + "\n" + "Uzvārds: " + uzvards + "\n"
+        + "Vecums: " + vecums + "\n" + "Vai bija nodarbībā? " + isPresent);
+
+        String teikums = String.format("Vārds: %s \n Uzvārds %s \n Vecums %s \n isPresent %s",vards,uzvards,vecums,isPresent);
+
+
+        System.out.println(teikums);
+    }
+}
